ad0d22c179c71aa2612e798060fbe9fec4c58ae8
naumanaziz <aziznauman96@hotmail.it>
2018-04-05 12:33:14 +0200

    init project

---
+++ NaumanAziz/App_Start/FilterConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace NaumanAziz {
+	public class FilterConfig {
+		public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
+			filters.Add(new HandleErrorAttribute());
+		}
+	}
+}
